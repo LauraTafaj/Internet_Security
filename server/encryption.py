ee7aea25589014d35f1311bf63c19debd194233b
MarigonaHamiti <115891512+marigonahamiti@users.noreply.github.com>
2024-05-22 22:05:31 +0200

    Update encryption.py

--- server/encryption.py
+++ server/encryption.py
@@ -1,1 +1,37 @@
 
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding, hashes
+from cryptography.hazmat.backends import default_backend
+import os
+
+class Encryptor:
+    def __init__(self, key):
+        self.key = key
+        self.backend = default_backend()
+        self.iv = os.urandom(16)
+
+    def encrypt(self, data):
+        padder = padding.PKCS7(128).padder()
+        padded_data = padder.update(data) + padder.finalize()
+        
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(self.iv), backend=self.backend)
+        encryptor = cipher.encryptor()
+        encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
+        return self.iv + encrypted_data
+
+    def decrypt(self, encrypted_data):
+        iv = encrypted_data[:16]
+        actual_encrypted_data = encrypted_data[16:]
+        
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv), backend=self.backend)
+        decryptor = cipher.decryptor()
+        padded_data = decryptor.update(actual_encrypted_data) + decryptor.finalize()
+
+        unpadder = padding.PKCS7(128).unpadder()
+        data = unpadder.update(padded_data) + unpadder.finalize()
+        return data
+
+    def hash(self, data):
+        digest = hashes.Hash(hashes.SHA256(), backend=self.backend)
+        digest.update(data)
+        return digest.finalize()
